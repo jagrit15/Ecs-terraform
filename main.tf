d917bbf26dbc0599f12c89ab7c64b5ea893a0d7d
Jagrit <106672304+jagrit15@users.noreply.github.com>
2025-07-28 04:03:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_cluster" "demo" {
 
 # IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecssssTaskExecutionRole"
+  name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
